b72d912c900e76ad08a5953185b9b3a43d4a5825
Frequency <rjt0266@gmail.com>
2025-01-31 16:44:52 +0530

    yarn convertion

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      },
-    },
-  },
-  define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
-  },
 })
