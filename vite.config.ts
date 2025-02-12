4879e91b2b7533fe75180f1a4d137aea0edd6526
Frequency <rjt0266@gmail.com>
2025-02-13 00:04:17 +0530

    base url fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default ({ mode }) => defineConfig({
   plugins: [react()],
-  base: '/not-a-temp-mail/',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
