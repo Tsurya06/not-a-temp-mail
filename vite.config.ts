6f862d25641319570b395f12789574d3266ffcae
Frequency <rjt0266@gmail.com>
2025-02-13 16:49:07 +0530

    old config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return defineConfig({
-    plugins: [react()],
-    base: './',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      emptyOutDir: true,
-    },
-    define: {
-      'import.meta.env.VITE_BASE_URL': JSON.stringify(env.VITE_BASE_URL),
-      'import.meta.env.VITE_STORAGE_KEY': JSON.stringify(env.VITE_STORAGE_KEY ),
-      'import.meta.env.VITE_PUBLIC_URL': JSON.stringify(env.VITE_PUBLIC_URL)
-    }
-  })
-}
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
+})
