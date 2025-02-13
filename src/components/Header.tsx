4ccc52933a2fc5f279928a7eb280746ba8fe57fd
Frequency <rjt0266@gmail.com>
2025-02-14 02:09:46 +0530

    package json build script changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export function Header({ onGenerateEmail, loading }: HeaderProps) {
       if (window.location.hostname.includes('github.io')) {
         // GitHub Pages environment
         const repoName = window.location.pathname.split('/')[1];
-        downloadUrl = `${window.location.origin}/${repoName}/dist/extension.zip`;
+        downloadUrl = `${window.location.origin}/${repoName}/extension.zip`;
       } else {
         // Local development environment
         downloadUrl = '/extension.zip';
