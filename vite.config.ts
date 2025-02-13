a2c9c35f3d6aea5493fe779142702be584c566bc
Frequency <rjt0266@gmail.com>
2025-02-13 13:20:58 +0530

    Revert "base url fix"
    This reverts commit 4879e91b2b7533fe75180f1a4d137aea0edd6526.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default ({ mode }) => defineConfig({
+export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  }
+  base: '/not-a-temp-mail/',
 })
